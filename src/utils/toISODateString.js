c5299ef96861d63452ca9eb90d272441af08a987
softdream815 <uyg0815@gmail.com>
2017-02-10 09:31:28 -0800

    Use consolidated-events for adding event listeners

--- src/utils/toISODateString.js
+++ src/utils/toISODateString.js
@@ -4,7 +4,7 @@ import toMomentObject from './toMomentObject';
 
 import { ISO_FORMAT } from '../../constants';
 
-export default function toISODateString(date, currentFormat) {
+export default function toLocalizedDateString(date, currentFormat) {
   const dateObj = moment.isMoment(date) ? date : toMomentObject(date, currentFormat);
   if (!dateObj) return null;
 
