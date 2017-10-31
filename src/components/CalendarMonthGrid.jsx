aad9278e548e56db82a4fe8603308799daa0b437
softdream815 <uyg0815@gmail.com>
2017-10-31 21:13:16 +0100

    Update README.md

--- src/components/CalendarMonthGrid.jsx
+++ src/components/CalendarMonthGrid.jsx
@@ -109,8 +109,6 @@ class CalendarMonthGrid extends React.Component {
     this.isTransitionEndSupported = isTransitionEndSupported();
     this.onTransitionEnd = this.onTransitionEnd.bind(this);
     this.setContainerRef = this.setContainerRef.bind(this);
-
-    this.locale = moment.locale();
   }
 
   componentDidMount() {
@@ -147,11 +145,6 @@ class CalendarMonthGrid extends React.Component {
     if (hasNumberOfMonthsChanged) {
       const withoutTransitionMonths = orientation === VERTICAL_SCROLLABLE;
       newMonths = getMonths(initialMonth, numberOfMonths, withoutTransitionMonths);
-    }
-
-    if (this.locale !== moment.locale()) {
-      this.locale = moment.locale();
-      newMonths = newMonths.map(m => m.locale(this.locale));
     }
 
     this.setState({
