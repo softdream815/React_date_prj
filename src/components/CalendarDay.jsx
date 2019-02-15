ce271ebe3b081d2f50c90ae0be1b9d539bcfc2ea
softdream815 <uyg0815@gmail.com>
2019-02-14 17:37:10 -0800

    add hovered-start-blocked-minimum-nights and hovered-start-blocked-first-possible-end modifiers

--- src/components/CalendarDay.jsx
+++ src/components/CalendarDay.jsx
@@ -79,9 +79,7 @@ class CalendarDay extends React.PureComponent {
   }
 
   onKeyDown(day, e) {
-    const {
-      onDayClick,
-    } = this.props;
+    const { onDayClick } = this.props;
 
     const { key } = e;
     if (key === 'Enter' || key === ' ') {
@@ -128,12 +126,13 @@ class CalendarDay extends React.PureComponent {
           modifiers.has('first-day-of-week') && styles.CalendarDay__firstDayOfWeek,
           modifiers.has('last-day-of-week') && styles.CalendarDay__lastDayOfWeek,
           modifiers.has('hovered-offset') && styles.CalendarDay__hovered_offset,
+          modifiers.has('hovered-start-first-possible-end') && styles.CalendarDay__hovered_start_first_possible_end,
+          modifiers.has('hovered-start-blocked-minimum-nights') && styles.CalendarDay__hovered_start_blocked_min_nights,
           modifiers.has('highlighted-calendar') && styles.CalendarDay__highlighted_calendar,
           modifiers.has('blocked-minimum-nights') && styles.CalendarDay__blocked_minimum_nights,
           modifiers.has('blocked-calendar') && styles.CalendarDay__blocked_calendar,
           hoveredSpan && styles.CalendarDay__hovered_span,
           modifiers.has('selected-span') && styles.CalendarDay__selected_span,
-          modifiers.has('last-in-range') && styles.CalendarDay__last_in_range,
           modifiers.has('selected-start') && styles.CalendarDay__selected_start,
           modifiers.has('selected-end') && styles.CalendarDay__selected_end,
           selected && !modifiers.has('selected-span') && styles.CalendarDay__selected,
@@ -254,14 +253,6 @@ export default withStyles(({ reactDates: { color, font } }) => ({
     },
   },
 
-  CalendarDay__last_in_range: {
-    borderStyle: 'solid',
-
-    ':hover': {
-      borderStyle: 'solid',
-    },
-  },
-
   CalendarDay__selected: {
     background: color.selected.backgroundColor,
     border: `1px double ${color.selected.borderColor}`,
@@ -332,6 +323,16 @@ export default withStyles(({ reactDates: { color, font } }) => ({
       border: `1px solid ${color.blocked_out_of_range.borderColor}`,
       color: color.blocked_out_of_range.color_active,
     },
+  },
+
+  CalendarDay__hovered_start_first_possible_end: {
+    background: color.core.borderLighter,
+    border: `1px double ${color.core.borderLighter}`,
+  },
+
+  CalendarDay__hovered_start_blocked_min_nights: {
+    background: color.core.borderLighter,
+    border: `1px double ${color.core.borderLight}`,
   },
 
   CalendarDay__selected_start: {},
